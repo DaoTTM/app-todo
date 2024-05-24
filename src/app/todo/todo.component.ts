40a53f1eddd109ab9fb8f567c437ed146a8e4512
DaoTTM <tranthimongdao12@gmail.com>
2024-05-24 11:02:00 +0700

    app-todo-cơ-ban

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  newTodo: string = '';
+  todos: string[] = [];
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push(this.newTodo.trim());
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(todo: string) {
+    this.todos = this.todos.filter(t => t !== todo);
+  }
+}
+
